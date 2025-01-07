9e8fd322c54f8306c9d856eefde51c46cd44a901
Julio <julio.garcia135@hotmail.com>
2025-01-07 10:53:42 -0600

    ip interna

--- src/utilis/api.js
+++ src/utilis/api.js
@@ -1,6 +1,7 @@
 const api = ({
-    addressEndpoints: 'https://www.backorders.chickenkiller.com',
+    addressEndpoints: 'https://34.169.26.104/',
 });
 //https://www.backorders.chickenkiller.com
 //http://localhost:4000
+//https://34.169.26.104/
 export default api;
